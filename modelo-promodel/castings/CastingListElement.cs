a23a8ae8c9bed01b179057986f6c588416234c6b
Oswaldo Guzman <oswaldo.dgmx@gmail.com>
2023-07-03 08:52:42 -0600

    Listado de castings

--- modelo-promodel/castings/CastingListElement.cs
+++ modelo-promodel/castings/CastingListElement.cs
@@ -1,12 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace promodel.modelo.castings;
 
-namespace promodel.modelo.castings
+/// <summary>
+/// DTO para despliegue de castings en la UI
+/// </summary>
+public class CastingListElement
 {
-    public class CastingListElement
-    {
-    }
+    /// <summary>
+    /// Identificador unico del casting
+    /// </summary>
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Nombre del castinf
+    /// </summary>
+
+    public string Nombre { get; set; }
+
+
+    /// <summary>
+    /// NOmbre del cliente para quien va el proyecto por ejemplo la emrpesa
+    /// </summary>
+    public string NombreCliente { get; set; }
+
+   
+    /// <summary>
+    /// FEcha opcional de apertura a los candidatos y revisores del proyecto
+    /// </summary>
+    public DateTime? FechaApertura { get; set; }
+
+    /// <summary>
+    /// Fecha opcional de cierre del proyecto 
+    /// </summary>
+    public DateTime? FechaCierre { get; set; }
+
+    /// <summary>
+    /// DEtrmina si acepta inscripcion por los modelos
+    /// </summary>
+    public bool AceptaAutoInscripcion { get; set; } = false;
+
+    /// <summary>
+    /// Indica si el proyecto se encuentra activo
+    /// </summary>
+    public bool Activo { get; set; } = true;
+
+    /// <summary>
+    /// Indica si el proyecto se marca como activo en base a su fecha de apertura
+    /// </summary>
+    public bool AperturaAutomatica { get; set; } = true;
+
+    /// <summary>
+    /// Especifica si el proyecto debe marcarse como inactivo al superarse la fecha de cierro
+    /// </summary>
+    public bool CierreAutomatico { get; set; } = true;
 }
---
+++ modelo-promodel/paginado/PaginaDatos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace promodel.modelo.paginado;
+
+public class PaginaDatos<T>
+{
+    /// <summary>
+    /// PAgina de datos actual comienza en cero
+    /// </summary>
+    public int Pagina { get; set; }
+
+    /// <summary>
+    /// Tamano de paginado
+    /// </summary>
+    public int Tamano { get; set; }
+
+    /// <summary>
+    /// Total de elementos en el repositorio
+    /// </summary>
+    public int Total { get; set; }
+
+    /// <summary>
+    /// Lista de elementos de la pagina actual
+    /// </summary>
+    public List<T> Elementos { get; set; }
+}
