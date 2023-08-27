d648c5a4665317133089a4e89e357f0eb9dc598a
Oswaldo DG <oswaldo.dgmx@gmail.com>
2023-08-27 16:31:23 -0600

    ModeloPerfilPublicoUsuario

---
+++ modelo-promodel/perfil/PerfilPublicoUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace promodel.modelo.perfil
+{
+    public class PerfilPublicoUsuario
+    {
+        /// <summary>
+        /// Identificador único del usuario
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Nombre del usuario para ser identificado por otros usuarios
+        /// </summary>
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Avatar del usuario en base 64 
+        /// </summary>
+        public string Avatar { get; set; }
+    }
+}
